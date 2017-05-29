292d344d279d1916d54dcdc0751d08d35bfdddbc
Andy Monroe <jayhawker805@gmail.com>
2017-05-29 17:00:52 -0500

    Update routes for admin/users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/index'
+  end
+
+  namespace :admin do
+    get 'users/create'
+  end
+
+  namespace :admin do
+    get 'users/new'
+  end
+
+  namespace :admin do
+    get 'users/edit'
+  end
+
+  namespace :admin do
+    get 'users/destroy'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :needs
 
