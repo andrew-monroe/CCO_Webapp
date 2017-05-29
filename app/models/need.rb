5f13156b519749fb10ec4a2be75e48522877545a
Andy Monroe <jayhawker805@gmail.com>
2017-05-29 17:00:31 -0500

    Tried to create a date range function (not working)

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,2 +1,10 @@
 class Need < ApplicationRecord
+
+  # def time_range(start_time, end_time)
+  #   if start_time.strftime("%Y:%m:%d") == end_time.strftime("%Y:%m:%d")
+  #     start_time.strftime("%b %d (%a) %I:%M%p-") + end_time.strftime("%I:%M%p")
+  #   else
+  #     start_time.strftime("%b %d (%a) %I:%M%p-") + end_time.strftime("%b %d (%a) %I:%M%p")
+  #   end
+  # end
 end
