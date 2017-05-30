ca34a5d606d94ace8989bafd6edb1ccdfb0556c2
Andy Monroe <jayhawker805@gmail.com>
2017-05-30 13:20:50 -0500

    Condense routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,9 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'users/index'
-  end
-
-  namespace :admin do
-    get 'users/create'
-  end
-
-  namespace :admin do
-    get 'users/new'
-  end
-
-  namespace :admin do
-    get 'users/edit'
-  end
-
-  namespace :admin do
-    get 'users/destroy'
-  end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :needs
+  namespace :admin do
+    resources :users
+  end
 
   root 'needs#index'
 end
