6ccae78b0f3c0543b0130b79ad875859aa29f1dc
Andy Monroe <jayhawker805@gmail.com>
2017-06-01 15:28:06 -0500

    More formatting changes

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,10 +1,13 @@
 class Need < ApplicationRecord
+  def time_range(start_time, end_time)
+    if start_time.strftime("%Y:%m:%d") == end_time.strftime("%Y:%m:%d")
+      start_time.strftime("%b %d (%a) %l:%M%p - ") + end_time.strftime("%l:%M%p")
+    else
+      start_time.strftime("%b %d (%a) %l:%M%p - ") + end_time.strftime("%b %d (%a) %l:%M%p")
+    end
+  end
 
-  # def time_range(start_time, end_time)
-  #   if start_time.strftime("%Y:%m:%d") == end_time.strftime("%Y:%m:%d")
-  #     start_time.strftime("%b %d (%a) %I:%M%p-") + end_time.strftime("%I:%M%p")
-  #   else
-  #     start_time.strftime("%b %d (%a) %I:%M%p-") + end_time.strftime("%b %d (%a) %I:%M%p")
-  #   end
-  # end
+  def get_recent
+    Need.find(:all, :order => "start_time desc", :limit => 10)
+  end
 end
