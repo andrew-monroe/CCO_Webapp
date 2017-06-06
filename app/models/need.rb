d9542443731ab6546e195ac819e4ee5dda592cad
Andy Monroe <jayhawker805@gmail.com>
2017-06-06 16:56:00 -0500

    Earmark(comment out) some obsolete methods for removal/deletion

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,15 +1,15 @@
 class Need < ApplicationRecord
-  def time_range(start_time, end_time)
-    if start_time.strftime("%Y:%m:%d") == end_time.strftime("%Y:%m:%d")
-      start_time.strftime("%b %d (%a) %l:%M%p - ") + end_time.strftime("%l:%M%p")
-    else
-      start_time.strftime("%b %d (%a) %l:%M%p - ") + end_time.strftime("%b %d (%a) %l:%M%p")
-    end
-  end
-
-  def formatted_datetime(time)
-    time.strftime("%A %B %e, %Y\n%l:%M%p")
-  end
+  # def time_range(start_time, end_time)
+  #   if start_time.strftime("%Y:%m:%d") == end_time.strftime("%Y:%m:%d")
+  #     start_time.strftime("%b %d (%a) %l:%M%p - ") + end_time.strftime("%l:%M%p")
+  #   else
+  #     start_time.strftime("%b %d (%a) %l:%M%p - ") + end_time.strftime("%b %d (%a) %l:%M%p")
+  #   end
+  # end
+  #
+  # def formatted_datetime(time)
+  #   time.strftime("%A %B %e, %Y\n%l:%M%p")
+  # end
 
   def get_recent
     Need.find(:all, :order => "start_time desc", :limit => 10)
