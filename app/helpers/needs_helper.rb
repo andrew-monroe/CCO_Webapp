bb236a94a2f394668fd90c699e611bf5f2ccd8ae
Andy Monroe <jayhawker805@gmail.com>
2017-06-14 14:48:39 -0500

    Add in button for coordinators to email volunteers

--- app/helpers/needs_helper.rb
+++ app/helpers/needs_helper.rb
@@ -1,2 +1,14 @@
 module NeedsHelper
+
+  def get_volunteers_emails(need)
+    email_list = ""
+    GalaxyNeedFetcher.new.get_volunteers_signed_up(need.galaxy_id).each do |volunteer|
+      email_list += volunteer['email']+" "
+    end
+    email_list
+  end
+
+  def get_email_subject_line(need)
+    need.title + " - " + need.formatted_start_datetime
+  end
 end
