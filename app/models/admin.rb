a996af644c6cf7ba5de352a76560ca258e2a49fd
Andy Monroe <jayhawker805@gmail.com>
2017-06-15 11:33:05 -0500

    Make admin sessions timeout

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,7 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :trackable, :validatable
+  devise :database_authenticatable, :timeoutable,
+         :recoverable, :trackable, :validatable,
+         :timeout_in => 30.minutes
 end
