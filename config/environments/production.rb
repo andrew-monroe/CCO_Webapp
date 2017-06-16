3bacaa226feb94dad6222eef2b8c06b96f2c6faf
Andy Monroe <jayhawker805@gmail.com>
2017-06-16 14:29:37 -0500

    Set production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,4 +100,6 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
+
+  config.action_mailer.default_url_options = { host: 'thawing-atoll-56720.herokuapp.com'}
 end
