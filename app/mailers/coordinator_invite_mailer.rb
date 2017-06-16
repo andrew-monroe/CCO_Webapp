96825365e8482e8c76a930be56681a5631bcd3d8
Andy Monroe <jayhawker805@gmail.com>
2017-06-16 13:25:46 -0500

    Fix typo

--- app/mailers/coordinator_invite_mailer.rb
+++ app/mailers/coordinator_invite_mailer.rb
@@ -2,10 +2,10 @@ class CoordinatorInviteMailer < ApplicationMailer
   require 'sendgrid-ruby'
   include SendGrid
 
-  def send_invite(@coordinator)
+  def send_invite(coordinator)
     from = Email.new(email: "#{ENV['SENDGRID_USERNAME']}")
     subject = 'Join the CCO team!'
-    to = Email.new(email: @coordinator.email)
+    to = Email.new(email: coordinator.email)
     content = Content.new(type: 'text/plain', value: 'Hello, Email!')
     mail = Mail.new(from, subject, to, content)
 
